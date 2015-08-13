e561adec14afae1feb3a6a8a990571d43279fb69
nrv <nherve@laposte.net>
2015-08-13 18:33:51 +0200

    size

--- src/main/java/fr/ina/research/amalia/model/tracking/TrackedObject.java
+++ src/main/java/fr/ina/research/amalia/model/tracking/TrackedObject.java
@@ -1,25 +1,25 @@
 /*
  * Copyright (c) 2015 Institut National de l'Audiovisuel, INA
  *
- * This file is free software: you can redistribute it and/or modify   
+ * This file is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or   
- * (at your option) any later version.                                 
- * 
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
  * Redistributions of source code and compiled versions
- * must retain the above copyright notice, this list of conditions and 
- * the following disclaimer.                                           
- * 
- * Neither the name of the copyright holder nor the names of its       
+ * must retain the above copyright notice, this list of conditions and
+ * the following disclaimer.
+ *
+ * Neither the name of the copyright holder nor the names of its
  * contributors may be used to endorse or promote products derived from
- * this software without specific prior written permission.            
- * 
- * You should have received a copy of the GNU Lesser General Public License   
- * along with this file. If not, see <http://www.gnu.org/licenses/>    
- * 
- * This file is distributed in the hope that it will be useful,        
- * but WITHOUT ANY WARRANTY; without even the implied warranty of      
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the        
+ * this software without specific prior written permission.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this file. If not, see <http://www.gnu.org/licenses/>
+ *
+ * This file is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU Lesser General Public License for more details.
  */
 package fr.ina.research.amalia.model.tracking;
@@ -31,7 +31,7 @@ import java.util.List;
 import fr.ina.research.amalia.model.jaxb.ShapeType;
 
 /**
- * 
+ *
  * @author Nicolas HERVE - nherve@ina.fr
  */
 public class TrackedObject extends TrackRange implements Iterable<TrackSegment> {
@@ -52,6 +52,10 @@ public class TrackedObject extends TrackRange implements Iterable<TrackSegment> 
 		return shapeType;
 	}
 
+	public boolean isEmpty() {
+		return seg.isEmpty();
+	}
+
 	@Override
 	public Iterator<TrackSegment> iterator() {
 		return seg.iterator();
@@ -63,6 +67,10 @@ public class TrackedObject extends TrackRange implements Iterable<TrackSegment> 
 
 	public void setShapeType(ShapeType shapeType) {
 		this.shapeType = shapeType;
+	}
+
+	public int size() {
+		return seg.size();
 	}
 
 	@Override
